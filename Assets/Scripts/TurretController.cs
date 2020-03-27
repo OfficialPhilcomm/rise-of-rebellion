05fca3288539c9cc88f50928a4e2fc8ba72792ff
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-03-27 14:18:53 +0100

    Fix naming

--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -5,42 +5,14 @@ using System.Collections.Generic;
 [RequireComponent(typeof(CannonController))]
 public class TurretController : MonoBehaviour
 {
-    public TurretData turretData;
-    public GameObject turretPrefab;
-
-    private Transform target = null;
-
-    private List<GameObject> turrets = new List<GameObject>();
-
-    // Use this for initialization
     void Start()
     {
-        InitializeTurrets();
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-    private void InitializeTurrets()
-    {
-        foreach(Vector3 position in turretData.positions)
-        {
-            GameObject turret = Instantiate(turretPrefab, position, Quaternion.identity);
-            turret.transform.parent = gameObject.transform;
-            turrets.Add(turret);
-        }
-    }
-
-    public void SetTarget(Transform target)
-    {
-        this.target = target;
-
-        foreach(GameObject turret in turrets)
-        {
-            // set target of every turret individually
-        }
     }
 }
---
+++ Assets/Scripts/TurretsManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TurretsManager : MonoBehaviour
+{
+    public TurretData turretData;
+    public GameObject turretPrefab;
+
+    private Transform target = null;
+
+    private List<GameObject> turrets = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+        InitializeTurrets();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void InitializeTurrets()
+    {
+        foreach (Vector3 position in turretData.positions)
+        {
+            GameObject turret = Instantiate(turretPrefab, position, Quaternion.identity);
+            turret.transform.parent = gameObject.transform;
+            turrets.Add(turret);
+        }
+    }
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+
+        foreach (GameObject turret in turrets)
+        {
+            // set target of every turret individually
+        }
+    }
+}
