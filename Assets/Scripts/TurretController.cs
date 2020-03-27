ab08a793ee2d7084c3f768f468d4259c579a4a53
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-03-27 13:51:18 +0100

    Add turret and cannon controller

---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CannonController))]
+public class TurretController : MonoBehaviour
+{
+    private Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+}
