59908a06d16509e21531ef7365abe1277358a65b
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-03-27 13:58:04 +0100

    Implement set target functionality

--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(CannonController))]
 public class TurretController : MonoBehaviour
 {
-    private Transform target;
+    private Transform target = null;
+
+    private List<GameObject> turrets = new List<GameObject>();
 
     // Use this for initialization
     void Start()
@@ -21,5 +24,10 @@ public class TurretController : MonoBehaviour
     public void SetTarget(Transform target)
     {
         this.target = target;
+
+        foreach(GameObject turret in turrets)
+        {
+            // set target of every turret individually
+        }
     }
 }
